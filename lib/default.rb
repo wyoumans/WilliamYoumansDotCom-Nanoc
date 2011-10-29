44768b68383c603c7f8945ab0a5b8edbdb3abe33
William Youmans <will@klanoma.com>
2011-10-29 15:01:34 -0600

    starting with HTML5

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,9 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+# To prevent cacheing the js and css on dev server
+# append a random query string to each path
+$random = '?' + rand(500000).to_s;
+
+# For output used on the TV
+#$random = '';
