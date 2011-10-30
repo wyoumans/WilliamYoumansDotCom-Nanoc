7a4d311a5cc43a10e3e3c703be65aec6f79d4100
William Youmans <will@klanoma.com>
2011-10-30 15:13:52 -0600

    ready to push to beta

--- output/scripts/functions.js
+++ output/scripts/functions.js
@@ -1,3 +1,5 @@
+var home_html = "";
+
 //document.ready
 $(function(){
   $("ul.tools img, footer #social img")
@@ -12,7 +14,8 @@ $(function(){
     e.preventDefault();
     var new_url = $(this).attr("id");
     var new_title = new_url.charAt(0).toUpperCase() + new_url.slice(1);
-    
+    var body_class = new_url;
+
     if(new_url == "index"){
       new_url = "";
     } else {
@@ -22,8 +25,15 @@ $(function(){
     console.log("User wants to go to this page: " + new_title);
     window.history.pushState(new_url, new_url, "/" + new_url);
     $("#content").stop(false, true).toggle("drop", 500, function() {
-      $("body").attr("id", new_url);
-      $("#content").html("<h1>" + new_title + "</h1><p><Coming soon, maybe/p>").toggle("drop", 500);
+      if($("body").attr("id") == "index"){
+        home_html = $("#content").html();
+      }
+      $("body").attr("id", body_class);
+      if(body_class == "index"){
+        $("#content").html(home_html).toggle("drop", 500);
+      } else {
+        $("#content").html("<h1>" + new_title + "</h1><p>Coming soon, maybe</p>").toggle("drop", 500);
+      }
     });
   });
 });
