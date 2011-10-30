8559642d64929e39c1d4653bcd77e3266d4cb22d
William Youmans <will@klanoma.com>
2011-10-30 02:56:30 -0600

    perfecting icons

--- content/scripts/functions.js
+++ content/scripts/functions.js
@@ -1,5 +1,11 @@
 //document.ready
 $(function(){
-
+  $("ul.tools img")
+  .mouseenter(function(){
+    $(this).attr("src", $(this).attr("src").replace(/-bw/, ""));
+  })
+  .mouseleave(function(){
+    $(this).attr("src", $(this).attr("src").replace(/\.png/, "-bw.png"));
+  });
 
 });
