e4aa3d783431235a3db031cf346d06dfc96743c5
William Youmans <will@klanoma.com>
2011-10-30 13:00:54 -0600

    cool html5 things

--- content/scripts/functions.js
+++ content/scripts/functions.js
@@ -8,4 +8,22 @@ $(function(){
     $(this).attr("src", $(this).attr("src").replace(/\.png/, "-bw.png"));
   });
 
+  $("nav a").click(function(e){
+    e.preventDefault();
+    var new_url = $(this).attr("id");
+    var new_title = new_url.charAt(0).toUpperCase() + new_url.slice(1);
+    
+    if(new_url == "index"){
+      new_url = "";
+    } else {
+      new_url = new_url + "/";
+    }
+
+    console.log("User wants to go to this page: " + new_title);
+    window.history.pushState(new_url, new_url, "/" + new_url);
+    $("#content").stop(false, true).toggle("drop", 500, function() {
+      $("body").attr("id", new_url);
+      $("#content").html("<h1>" + new_title + "</h1>").toggle("drop", 500);
+    });
+  });
 });
