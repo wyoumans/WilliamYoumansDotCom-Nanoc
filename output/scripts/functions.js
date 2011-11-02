d0653d1be6db08e51e5a2bc959fdfc0056033c45
William Youmans <will@klanoma.com>
2011-11-02 00:15:36 -0600

    livequery

--- output/scripts/functions.js
+++ output/scripts/functions.js
@@ -27,10 +27,10 @@ $(function(){
   ]);
 
   $("ul.tools img, footer #social img")
-  .mouseenter(function(){
+  .livequery("mouseenter", function(){
     $(this).attr("src", $(this).attr("src").replace(/-bw/, ""));
   })
-  .mouseleave(function(){
+  .livequery("mouseleave", function(){
     $(this).attr("src", $(this).attr("src").replace(/\.png/, "-bw.png"));
   });
 
