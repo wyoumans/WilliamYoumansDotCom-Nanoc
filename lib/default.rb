6e6457ffa2b522711df3e11c6ca426e0652033c6
William Youmans <will@klanoma.com>
2011-11-05 05:15:54 -0600

    GA

--- lib/default.rb
+++ lib/default.rb
@@ -11,18 +11,6 @@ include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::Text
 include Nanoc3::Helpers::XMLSitemap
 
-class Nanoc3::Item
-  def content(opts = {})
-    opts[:rep] ||= :default
-    opts[:snapshot] ||= :last
-    reps.find { |r| r.name == opts[:rep] }.content_at_snapshot(opts[:snapshot])
-  end
-
-  def name
-    identifier.split("/").last 
-  end
-end
-
 $devserver = true
 
 if $devserver
