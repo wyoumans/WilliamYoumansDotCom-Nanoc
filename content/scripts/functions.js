62742343ed352a934bfbba92a6dbfd39286e2e06
William Youmans <will@klanoma.com>
2011-11-05 04:39:30 -0600

    changing browser titles

--- content/scripts/functions.js
+++ content/scripts/functions.js
@@ -1,3 +1,6 @@
+var animation_effect = "drop";
+var animation_speed = "slow";
+
 //document.ready
 $(function(){
   preload([
@@ -35,12 +38,16 @@ $(function(){
   $("nav a").click(function(e){
     e.preventDefault();
     var new_url = $(this).data("url");
-    var new_title = new_url.charAt(0).toUpperCase() + new_url.slice(1);
+    var new_title = "Web Developer, World Traveler"
+    if(new_url != "index"){
+      new_title = new_url.charAt(0).toUpperCase() + new_url.slice(1);
+    }
+
     var body_id = new_url;
     if($("body").attr("id") == new_url)
       return;
 
-    $("#content_ajax").stop(false, true).toggle("drop", "fast", function() {
+    $("#content_ajax").stop(false, true).toggle(animation_effect, animation_speed, function() {
       //$("#ajax_loading").show();
 
       if(new_url == "index"){
@@ -50,12 +57,12 @@ $(function(){
       }
 
       window.history.pushState(new_url, new_url, "/" + new_url);
-
+      document.title = document.title.replace(/^(.*)\|.*$/, "$1 | " + new_title);
       $("body").attr("id", body_id);
-      
+
       $.get("/ajax/" + body_id + ".html", function(data){
         //$("#ajax_loading").hide();
-        $("#content_ajax").html(data).toggle("drop", "fast");
+        $("#content_ajax").html(data).toggle(animation_effect, animation_speed);
       });
     });
   });
