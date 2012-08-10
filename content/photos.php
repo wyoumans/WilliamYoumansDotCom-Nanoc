330d9e3653e97057b106c2d0b6bf3f5ccb2b6a25
William Youmans <will@williamyoumans.com>
2012-08-10 15:24:52 -0600

    parse php as haml for clean html

--- content/photos.php
+++ content/photos.php
@@ -6,19 +6,19 @@ url: photos
 %h1
   Photos
 
-<?php
+:plain
+  <?php
 
-//pull most recent blog post from rss
-$doc = new DOMDocument();
-$doc->load('http://klanoma.tumblr.com/rss/');
-$feed = array();
-foreach ($doc->getElementsByTagName('item') as $node) {
-  $feed[] = array(
-    'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
-    'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
-    'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
-  );
-}
+  $doc = new DOMDocument();
+  $doc->load('http://klanoma.tumblr.com/rss/');
+  $feed = array();
+  foreach ($doc->getElementsByTagName('item') as $node) {
+    $feed[] = array(
+      'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+      'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+      'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+    );
+  }
 
-var_dump($feed);
-?>
+  var_dump($feed);
+  ?>
