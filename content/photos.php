762cb88008db3fb1d5f4944fb37624c8a8c2a9fa
William Youmans <will@williamyoumans.com>
2012-08-10 15:05:18 -0600

    added xml parsing code

--- content/photos.php
+++ content/photos.php
@@ -9,4 +9,18 @@ url: photos
 %iframe{:src => 'http://klanoma.tumblr.com/'}
 
 
-<?php echo 'Ha! We have PHP!'; ?>
+<?php
+
+//pull most recent blog post from rss
+$doc = new DOMDocument();
+$doc->load('http://klanoma.tumblr.com/rss/');
+$feed = array();
+foreach ($doc->getElementsByTagName('item') as $node) {
+  $feed[] = array(
+    'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+    'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+    'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+  );
+}
+
+?>
