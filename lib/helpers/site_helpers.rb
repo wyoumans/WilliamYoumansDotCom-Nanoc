51fe27b448683c6b79a7e72c806e5ed39b463d16
William Youmans <will@klanoma.com>
2012-08-11 13:31:10 -0600

    added coffeescript and some helpful functions

---
+++ lib/helpers/site_helpers.rb
@@ -0,0 +1,14 @@
+module SiteHelpers
+
+  def getCurrentYear
+    Time.now.year
+  end
+
+  def isProduction
+    false
+  end
+
+  def getTimestampQueryString
+    '?' + Time.now.to_i.to_s;
+  end
+end
---
+++ lib/helpers_.rb
@@ -0,0 +1,7 @@
+# By the way, this filename ends in an underscore for a reason.
+# It needs to be loaded after the helpers folder
+
+include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::LinkTo
+
+include SiteHelpers
