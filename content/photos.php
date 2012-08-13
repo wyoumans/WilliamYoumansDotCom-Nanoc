ebc6726b5fcc60c348f1637dd51447fae7c53975
William Youmans <will@williamyoumans.com>
2012-08-13 09:50:58 -0600

    updated form haml

--- content/photos.php
+++ content/photos.php
@@ -6,27 +6,23 @@ url: photos
 %h1
   Photos
 
-:plain
-  <?php
-  date_default_timezone_set('America/Denver');
-  $doc = new DOMDocument();
-  $doc->load('http://klanoma.tumblr.com/rss/');
-  $count = 0;
-  ?>
-  <?php foreach ($doc->getElementsByTagName('item') as $node): ?>
-    <div class="post">
-      <h2>
-        <span>
-          <?php echo date('m/d/Y', strtotime($node->getElementsByTagName('pubDate')->item(0)->nodeValue)) ?>
-        </span>
-      </h2>
-      <p>
-        <?php echo $node->getElementsByTagName('description')->item(0)->nodeValue ?>
-      </p>
-    </div>
-    <?php $count++; ?>
-    <?php if($count > 5) break; ?>
-  <?php endforeach; ?>
+<?php
+date_default_timezone_set('America/Denver');
+$doc = new DOMDocument();
+$doc->load('http://klanoma.tumblr.com/rss/');
+$count = 0;
+?>
+<?php foreach ($doc->getElementsByTagName('item') as $node): ?>
+
+.post
+  %h2
+    %span <?php echo date('m/d/Y', strtotime($node->getElementsByTagName('pubDate')->item(0)->nodeValue)) ?>
+
+  %p <?php echo $node->getElementsByTagName('description')->item(0)->nodeValue ?>
+
+<?php $count++; if($count > 5) break; ?>
+<?php endforeach; ?>
+
 
 .post
   %a{:href => 'http://klanoma.tumblr.com', :target => '_blank'} See More
